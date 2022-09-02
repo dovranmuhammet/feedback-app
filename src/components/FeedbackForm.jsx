b0622700be880e1f5a8fb4f72efb972fae2ad317
Dovran <dovranrustemov@gmail.com>
2022-09-02 13:03:11 -0400

    Added forms,validitaion, simple animation

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us ?</h2>
+        {/* @todo - rating select Component */}
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            value={text}
+            type='text'
+            placeholder='Write a review'
+          />
+          <Button type='submit'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
