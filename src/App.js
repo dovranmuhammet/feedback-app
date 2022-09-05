c837f64b7abbebed3421dc73b44887f2ef32220a
Dovran <dovranrustemov@gmail.com>
2022-09-05 15:49:53 -0400

    Added all needs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react'
-import FeedbackForm from './components/FeedbackForm'
-import FeedbackItem from './components/FeedbackItem'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
-import Header from './components/Header'
-import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
-const App = () => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete ?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
+function App() {
   return (
-    <>
-      <Header />
-      <div className='container'>
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 function Header({ text, bgColor, textColor }) {
   const headerStyles = {
@@ -9,7 +10,9 @@ function Header({ text, bgColor, textColor }) {
   return (
     <header style={headerStyles}>
       <div className='container'>
-        <h2>{text}</h2>
+        <Link to='/' style={{ textDecoration: 'none', color: '#ff6a95' }}>
+          <h2>{text}</h2>
+        </Link>
       </div>
     </header>
   )
@@ -17,7 +20,7 @@ function Header({ text, bgColor, textColor }) {
 
 Header.defaultProps = {
   text: 'Feedback UI',
-  bgColor: 'rgba(0, 0, 0, 0.4)',
+  bgColor: 'rgba(0,0,0,0.4)',
   textColor: '#ff6a95',
 }
 
